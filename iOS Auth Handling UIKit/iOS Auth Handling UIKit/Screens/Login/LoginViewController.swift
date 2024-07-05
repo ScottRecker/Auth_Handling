89fc75b6f0f9b4ffe34756657d47bee57af3999c
Scott Recker <scott.a.recker@gmail.com>
2024-07-05 10:51:08 -0500

    Finish login view controller

--- iOS Auth Handling UIKit/iOS Auth Handling UIKit/Screens/Login/LoginViewController.swift
+++ iOS Auth Handling UIKit/iOS Auth Handling UIKit/Screens/Login/LoginViewController.swift
@@ -7,14 +7,24 @@
 
 import UIKit
 
+protocol LoginNavDelegate: AnyObject {
+    func onRegisterTapped()
+    func onLoginSuccessful()
+}
+
 class LoginViewController: UIViewController {
+    private lazy var emailTextField = UITextField.makeEmailField()
+    private lazy var passwordTextField = UITextField.makePasswordField()
+    private lazy var submitButton = UIButton.makeButton(title: "Submit", target: self, action: #selector(self.onSubmitTapped))
+    private lazy var registerButton = UIButton.makeButton(title: "Register", target: self, action: #selector(self.onRegisterTapped))
+
+    weak var navDelegate: LoginNavDelegate?
 }
 
 // MARK: - Lifecycle
 extension LoginViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-
         setupViews()
     }
 }
@@ -22,19 +32,41 @@ extension LoginViewController {
 // MARK: - View Setup/Configuration
 private extension LoginViewController {
     func setupViews() {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Hello World!"
+        title = "Login"
         view.backgroundColor = .white
 
-        view.addSubview(label)
+        emailTextField
+            .addTo(view)
+            .centerOn(view)
+            .setDefaultFieldSize(superview: view)
+        
+       passwordTextField
+            .addTo(view)
+            .centerXOn(view)
+            .pinTop(toAnchor: emailTextField.bottomAnchor, constant: 10)
+            .setDefaultFieldSize(superview: view)
 
-        NSLayoutConstraint.activate([
-            label.heightAnchor.constraint(equalToConstant: 50.0),
-            label.widthAnchor.constraint(equalToConstant: 100.0),
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
+        submitButton
+            .addTo(view)
+            .centerXOn(view)
+            .pinTop(toAnchor: passwordTextField.bottomAnchor, constant: 10)
+
+        registerButton
+            .addTo(view)
+            .centerXOn(view)
+            .pinBottom(toAnchor: view.safeAreaLayoutGuide.bottomAnchor, constant: -10)
+    }
+}
+
+// MARK: - Actions
+extension LoginViewController {
+
+    @objc func onSubmitTapped() {
+        navDelegate?.onLoginSuccessful()
+    }
+
+    @objc func onRegisterTapped() { 
+        navDelegate?.onRegisterTapped()
     }
 }
 
