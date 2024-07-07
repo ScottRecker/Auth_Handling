20683795fd65bfb185ab5cbe3d6adb7dc9c63c82
Scott Recker <scott.a.recker@gmail.com>
2024-07-07 10:47:19 -0500

    Add network handler

---
+++ iOS Auth Handling UIKit/iOS Auth Handling UIKit/Model Layer/Network/SecureFetchData.swift
@@ -0,0 +1,13 @@
+//
+//  SecureFetchData.swift
+//  iOS Auth Handling UIKit
+//
+//  Created by Scott Recker on 7/5/24.
+//
+
+import Foundation
+
+struct SecureFetchData: Codable {
+    let message: String
+    
+}
